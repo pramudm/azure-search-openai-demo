01276d1e031479f72267c449122843f3ed85806f
pramudm <156995857+pramudm@users.noreply.github.com>
2024-01-29 03:41:12 +0000

    "new files"

--- app/frontend/src/pages/oneshot/OneShot.tsx
+++ app/frontend/src/pages/oneshot/OneShot.tsx
@@ -173,7 +173,7 @@ export function Component(): JSX.Element {
                 <h1 className={styles.oneshotTitle}>Ask your data</h1>
                 <div className={styles.oneshotQuestionInput}>
                     <QuestionInput
-                        placeholder="Example: Does my plan cover annual eye exams?"
+                        placeholder="Example: what are the main challenges the government faced in 2019?"
                         disabled={isLoading}
                         initQuestion={question}
                         onSend={question => makeApiRequest(question)}
